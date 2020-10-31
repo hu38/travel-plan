959bb13f5ddbcba263d0307a724cc36afed86cc5
LINGAP <lma1169949842@gmail.com>
2020-10-31 12:09:47 -0500

    recommended api extracted http request method and added AppConfig class as container for beans

--- backend/travel/src/main/java/com/travelPlanner/travel/model/AttractionsGoogleAPIResponse/Attraction.java
+++ backend/travel/src/main/java/com/travelPlanner/travel/model/AttractionsGoogleAPIResponse/Attraction.java
@@ -2,17 +2,26 @@ package com.travelPlanner.travel.model.AttractionsGoogleAPIResponse;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.travelPlanner.travel.model.CityGoogleAPIResponse.Geometry;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Attraction {
-    public String name;
+    public String business_status;
 
     @JsonProperty("formatted_address")
-    public String address;
+    public String formattedAddress;
+
+    public Geometry geometry;
+
+    public String name;
+
+    public Photo[] photos;
 
     @JsonProperty("place_id")
     public String placeID;
 
     public float rating;
+
+    public int user_ratings_total;
 
 }
--- backend/travel/src/main/java/com/travelPlanner/travel/model/AttractionsGoogleAPIResponse/Photo.java
+++ backend/travel/src/main/java/com/travelPlanner/travel/model/AttractionsGoogleAPIResponse/Photo.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class AttractionsGoogleAPIResponse {
-
-    @JsonProperty("next_page_token")
-    public String nextPageToken;
-
-    public Attraction[] results;
-
+public class Photo {
+    @JsonProperty("photo_reference")
+    public String photoReference;
+    public int width;
 }
