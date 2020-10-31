c108bd944c47db17b39c6cb411dbb9a47873dc2f
WEIJING SONG <weijingsong@WEIJINGs-MBP.attlocal.net>
2020-10-31 14:49:16 -0700

    frontend folder reorganized

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Main from './frontend/Components/Main'
+import Main from '../src/components/Main'
 
 ReactDOM.render(
   <React.StrictMode>
