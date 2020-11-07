04aa1ac9d89de0d2c67d57ee66de40dc5fd62ffb
LINGAP <lma1169949842@gmail.com>
2020-11-07 10:24:21 -0600

    db - added user model, user service, user controller, and userDao. untested. Need Plan field.

---
+++ backend/travel/src/main/java/com/travelPlanner/travel/dao/UserDao.java
@@ -0,0 +1,56 @@
+package com.travelPlanner.travel.dao;
+
+import com.travelPlanner.travel.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+@Repository
+public class UserDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void addUser(User user){
+        user.setEnabled(true);
+
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+
+            session.save(user);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            assert session != null;
+            session.getTransaction().rollback();
+            throw e;
+        }finally {
+            if (session != null){
+                session.close();
+            }
+        }
+    }
+
+    public User getUserById(String id){
+        User user;
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+            criteriaQuery.select(root).where(builder.equal(root.get("user_id"), id));
+            user = session.createQuery(criteriaQuery).getSingleResult();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+        return user;
+    }
+}
--- backend/travel/src/main/java/com/travelPlanner/travel/model/User.java
+++ backend/travel/src/main/java/com/travelPlanner/travel/model/User.java
@@ -1,7 +1,41 @@
 package com.travelPlanner.travel.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
+    private static final long serialVersionUID = 2681531852204068105L;
 
+    @Id
+    @Column(name = "user_id")
+    private String id;
+
+    private String password;
+    private boolean enabled;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
