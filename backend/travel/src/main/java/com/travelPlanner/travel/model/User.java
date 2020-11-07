1b10615dc17867ab9e9d45a2f3fb8ffc7bf6680f
LINGAP <lma1169949842@gmail.com>
2020-11-07 10:24:21 -0600

    db

--- backend/travel/src/main/java/com/travelPlanner/travel/model/User.java
+++ backend/travel/src/main/java/com/travelPlanner/travel/model/User.java
@@ -1,7 +1,49 @@
 package com.travelPlanner.travel.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 2681531852204068105L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String emailId;
+    private String password;
+    private boolean enabled;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
