06cb0992318966923d2b0daa9ef543672795d429
LINGAP <lma1169949842@gmail.com>
2020-11-10 21:49:08 -0600

    login and register ok

--- backend/travel/src/main/java/com/travelPlanner/travel/dao/UserDao.java
+++ backend/travel/src/main/java/com/travelPlanner/travel/dao/UserDao.java
@@ -45,7 +45,7 @@ public class UserDao {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
             Root<User> root = criteriaQuery.from(User.class);
-            criteriaQuery.select(root).where(builder.equal(root.get("user_id"), id));
+            criteriaQuery.select(root).where(builder.equal(root.get("id"), id));
             user = session.createQuery(criteriaQuery).getSingleResult();
         }catch (Exception e){
             e.printStackTrace();
