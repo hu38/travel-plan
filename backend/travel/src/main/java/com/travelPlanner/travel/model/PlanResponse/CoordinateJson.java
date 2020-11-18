74d8761c377af74502f0e561c41923f74387da07
gloriaswj <gloriaswj@users.noreply.github.com>
2020-11-18 13:23:33 -0800

    latest changes

--- backend/travel/src/main/java/com/travelPlanner/travel/model/PlanResponse/CoordinateJson.java
+++
@@ -1,6 +0,0 @@
-package com.travelPlanner.travel.model.PlanResponse;
-
-public class CoordinateJson {
-    public String lat;
-    public String lng;
-}
--- backend/travel/src/main/java/com/travelPlanner/travel/model/PlanResponse/ReadPlanResponseBody.java
+++
@@ -1,7 +0,0 @@
-package com.travelPlanner.travel.model.PlanResponse;
-
-public class ReadPlanResponseBody {
-    public String planID;
-    public String statusMessage;
-    public PlanData data;
-}
